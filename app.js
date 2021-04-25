4afb7306945a9052543f1d0c0c4103d3cda430a6
[Andrew Core] <[adcore89@gmail.com]>
2021-04-24 20:19:15 -0400

    started to capture user input

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log ('hello');
+// user input test 
+
+const readmeDataArgs = process.argv.slice(2, process.argv.length);
+console.log(readmeDataArgs);
+
+const printReadmeData = readmeDataArr => {
+    for (let i = 0; i < readmeDataArr.length; i++) {
+        console.log(readmeDataArr[i]);
+    }
+};
